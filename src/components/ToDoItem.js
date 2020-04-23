7622facd893381449dce4a41f0fc11481f6611b1
Ferdy Macleod <ferdymacleod21@gmail.com>
2020-04-23 14:46:02 +1000

    added lot of styling and todoitem component

---
+++ src/components/ToDoItem.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import { Card, CardContent, Checkbox } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
+
+class ToDoItem extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            checked: false,
+        }
+    }
+
+    handleChange() {
+        this.setState({
+            checked: !this.state.checked
+        })
+    }
+
+    render() {
+        return (
+            <Card className="to-do-item">
+
+                <div className="to-do-item-box-1">
+                    <Typography variant="h5">{this.props.name}</Typography>
+                </div>
+                <div className="to-do-item-box-2">
+                    <Typography>{this.props.task}</Typography>
+                </div>
+                <Checkbox className="to-do-item-checkbox" color="primary" onChange={this.handleChange.bind(this)}/>
+
+            </Card>
+        )
+    }
+}
+
+export default ToDoItem;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,46 +1,90 @@
 import React, { Component} from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import { Fab, Card, CardContent, CardActions, TextField, Button, Typography, Box } from '@material-ui/core'
+import { Fab, Card, TextField, Button, Typography,} from '@material-ui/core'
 import AddIcon from '@material-ui/icons/Add'
+import CloseIcon from '@material-ui/icons/Close'
+import ToDoItem from './ToDoItem'
 import '../App.css'
 
 
 class ToDoList extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            addItemCardTriggered: false,
+        }
     }
 
+    activateAddItemCard = () => {
+        this.setState({
+            addItemCardTriggered: true,
+        })
+    }
+
+    deActivateAddItemCard() {
+        this.setState({
+            addItemCardTriggered: false,
+        })
+    }
     render() {
         return (
             <>
-            <div className="add-button">
-                <Fab color="primary" aria-label="add">
-                    <AddIcon />
-                </Fab>
-            </div>
-                <AddItemCard />
+            
+            {this.state.addItemCardTriggered ? (
+                <>
+                    <AddItemCard />
+                    <div className="add-button">
+                    <Fab color="primary" aria-label="add" onClick={() => this.deActivateAddItemCard()}>
+                        
+                        <CloseIcon />
+                    </Fab>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div className="add-button">
+                    <Fab color="primary" aria-label="add" onClick={() => this.activateAddItemCard()}>
+                        
+                        <AddIcon />
+                    </Fab>
+                    </div>
+                    <ToDoItem name="example to do item" task="example task" />
+                    <ToDoItem name="example number two" task="example task" />
+                    <ToDoItem name="Do the laundry" task="Make sure to do all of laundry and put it on the line after" />
+                </>
+            )}
+                
             </>
         )
     }
 } 
 
 
-function AddItemCard() {
-  
-    return (
-      <Card className="add-item-card">
+class AddItemCard extends Component {
+    
+    constructor(props) {
+        super(props)
+    }
 
-        <Typography className="add-item-title" variant="h5" >Add To-Do Item</Typography>
-        <form className="add-item-form">
-            <TextField id="standard-basic" label="Name" variant="outlined" />
-            <TextField id="standard-basic" label="Task" variant="outlined" />
-        </form>
-
-        <div className="add-item-button-box">
-            <Button className="add-item-button" variant="contained" color="primary">Add Item</Button>
-        </div>
-
-      </Card>
-    );
+    render() {
+        return (
+          <Card className="add-item-card">
+              <div className="add-item-title-container">
+                <div className="add-item-title">
+                    <Typography variant="h5" >Add To-Do Item</Typography>
+                </div>
+    
+              </div>
+            <form className="add-item-form">
+                <TextField id="standard-basic" label="Name" variant="outlined" />
+                <TextField id="standard-basic" label="Task" variant="outlined" />
+            </form>
+    
+            <div className="add-item-button-box">
+                <Button className="add-item-button" variant="contained" color="primary">Add Item</Button>
+            </div>
+    
+          </Card>
+        );
+    }
   }
 export default ToDoList;
