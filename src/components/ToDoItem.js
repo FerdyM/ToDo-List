f1d91e11edf31acbd2bdaa1b03d3441d5805dd6c
Ferdy Macleod <ferdymacleod21@gmail.com>
2020-04-27 13:59:13 +1000

    added a dot thingy so menu is hidden unless u need it

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
-import { Card, CardContent, Checkbox, Button } from '@material-ui/core'
+import { Card, CardContent, Checkbox, Button, ButtonGroup } from '@material-ui/core'
 import { Typography } from '@material-ui/core'
+import  MoreVertIcon from '@material-ui/icons/MoreVert'
+import CloseIcon from '@material-ui/icons/Close'
 import axios from 'axios'
 
 const api = axios.create({
@@ -12,9 +14,15 @@ class ToDoItem extends Component {
         super(props)
         this.state = {
             checked: false,
+            menu: true
         }
     }
     
+    toggleMenu() {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
 
     handleChange() {
         this.setState({
@@ -46,9 +54,19 @@ class ToDoItem extends Component {
                     <Typography>{this.props.task}</Typography>
                 </div>
                 <div className="to-do-item-checkbox">
-                    <Checkbox size='medium'  color="primary" onChange={this.handleChange.bind(this)} onClick={this.logItem}/>
-                    <Button variant="contained" color="primary" onClick={this.deleteItem}>Delete me</Button>
-                    <Button onClick={this.editItem}>Edit</Button>
+                   
+                    <Checkbox size='medium'  color="primary" onChange={this.handleChange.bind(this)}/>
+                    {this.state.menu ? 
+                        (<MoreVertIcon onClick={this.toggleMenu.bind(this)}/>) 
+                        : (
+                            <>
+                                <CloseIcon onClick={this.toggleMenu.bind(this)}/>
+                                <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+                                    <Button variant="contained" color="primary" onClick={this.deleteItem}>Delete</Button>
+                                    <Button onClick={this.editItem}>Edit</Button>
+                                </ButtonGroup>
+                            </>
+                        )}
                 </div>
             </Card>
         )
